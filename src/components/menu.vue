a4f6b5da613a7cb55463ff080666450da9e20736
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-09-30 11:27:19 +0200

    Add link menu

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,6 +1,6 @@
 <template>
     <ul>
-        <div class="text-black flex flex-col gap-10 mt-36 mr-5 fixed right-0">
+        <div class="text-black flex flex-col gap-6 mt-32 mr-5 fixed right-0">
             <router-link class="link text-[#3eb1ce] hover:w-56 hover:h-14 transition-all w-48 h-10 justify-center items-center flex border-2 border-white bg-[#222] no-underline hover:text-xl" to="/simple">
                 <code class="font-mono">Simple</code>
             </router-link>
@@ -24,6 +24,9 @@
             </router-link>
             <router-link class="link text-[#3eb1ce] hover:w-56 hover:h-14 transition-all w-48 h-10 justify-center items-center flex border-2 border-white bg-[#222] no-underline hover:text-xl" to="/edit/id ">
                 <code class="font-mono">Modifier</code>
+            </router-link>
+            <router-link class="link text-[#3eb1ce] hover:w-56 hover:h-14 transition-all w-48 h-10 justify-center items-center flex border-2 border-white bg-[#222] no-underline hover:text-xl" to="/quartier/new ">
+                <code class="font-mono">Créer quartier</code>
             </router-link>
         </div>
     </ul>
